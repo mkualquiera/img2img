05ddee73b15274876dd38fff9ccb5421eb628b63
Juan Pablo Ossa Zapata <jpossaz@apolo.eafit.edu.co>
2023-02-09 23:23:13 -0500

    [ scripts ] Updated slurm scripts

--- train.sh
+++ train.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-#SBATCH --partition=accel-2
+#SBATCH --partition=accel
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=01:00:00
 #SBATCH --job-name=text_embedding
-#SBATCH --output=logs/job_%j.out
-#SBATCH --error=logs/job_%j.err
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
 #SBATCH --mem=32GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jpossaz@eafit.edu.co
+
+echo "Starting job"
 
 source ~/.bashrc
 
 conda activate img2img
 
-# If we are running in slurm, we need to set the run name to the job id,
-# otherwise we need to set it to a random value
 if [ -z "$SLURM_JOB_ID" ]; then
     export SLURM_JOB_ID=$(python -c "import random; print(random.randint(0, 1000000000000000))")
 fi
 
-python -m cProfile -o perf.prof -m img2img.training \
+python -m img2img.training \
     --run_name $SLURM_JOB_ID \
     --data_path data_files/downloaded/laion6p0 \
     --model_config_path assets/configs/base.json \
